47f3a7b54e1e1e570ff456fdec5ce64e1747f53a
Arai0913 <araidororo22@gmail.com>
2024-05-20 02:15:07 +0900

    水面shaderの追加

---
+++ Big Wave prototype/Assets/Script/EtcScript/RopeEffect.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeEffect : MonoBehaviour
+{
+    Enemy enemy;
+    Player player;
+    LineRenderer lineRenderer; // LineRendererコンポーネント
+
+    [SerializeField] GameObject startPoint;//ロープの始点
+    [SerializeField] GameObject endPoint;//ロープの終点
+    public GameObject[] vertices = new GameObject[20];//ロープの質点
+
+    void Start()
+    {
+        enemy = GameObject.FindWithTag("Enemy").GetComponent<Enemy>();
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.positionCount = vertices.Length;
+
+        foreach (GameObject v in vertices)
+        {
+            v.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (enemy.hp > 0 && player.hp > 0)
+        {
+            DrawRope();
+        }
+
+        else
+        {
+            lineRenderer.positionCount = 0;//ロープの描写をなくす
+        }
+    }
+
+    void DrawRope()
+    {
+        Vector3 enemyPosition = enemy.transform.position;//敵の座標を取得
+        Vector3 playerPosition = player.transform.position;//プレイヤーの座標を取得
+        enemyPosition.z -= enemy.transform.localScale.z / 2f;
+        playerPosition.z += player.transform.localScale.z / 2f;
+
+        startPoint.transform.position = playerPosition;//ロープの視点の座標にプレイヤーの座標に移動
+        endPoint.transform.position = enemyPosition;//ロープの終点の座標を敵の座標に移動
+
+        int index = 0;
+        foreach (GameObject v in vertices)
+        {
+            lineRenderer.SetPosition(index, v.transform.position);  // 質点の座標を設定
+            index++;
+        }
+    }
+}
--- Big Wave prototype/Assets/Script/PlayerScript/AttackControl.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/AttackControl.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 public class AttackControl : MonoBehaviour
 {
-    [SerializeField] float damageMax = 1f;//最大ダメージ(トリックを最大まで溜めた時のダメージ)
+    [SerializeField] float strong_TrickCostPercent=50;//強攻撃時の消費トリック(プレイヤーの最大トリックのstrong_TrickCostPercent%分消費)
+    [SerializeField] float medium_TrickCostPercent=30;//中攻撃時の消費トリック(プレイヤーの最大トリックのmedium_TrickCostPercent%分消費)
+    [SerializeField] float weak_TrickCostPercent=10;//弱攻撃時の消費トリック(プレイヤーの最大トリックのweak_TrickCostPercent%分消費)
+    [SerializeField] float strong_Damage = 100;//強攻撃時の敵に与えるダメージ
+    [SerializeField] float medium_Damage = 60;//中攻撃時の敵に与えるダメージ
+    [SerializeField] float weak_Damage = 20;//弱攻撃時の敵に与えるダメージ
+    private float trickCost;//消費トリック
     Enemy enemy;
     Player player;
     JumpControl jumpcontrol;
@@ -21,28 +27,57 @@ public class AttackControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            Attack();//攻撃
+        Attack_Strong();//強攻撃(ゲージ消費大)
+        Attack_Medium();//中攻撃(ゲージ消費中)
+        Attack_Weak();//弱攻撃(ゲージ消費小)
+        AttackedtoFalseNoJump();//ジャンプしていない時攻撃していない判定にする
     }
 
-    //攻撃(ジャンプ中に左クリック)
-    //trickを消費して敵にダメージを与える、trickの値によってダメージが変わる(ダメージの値はtrick×damagecoefficient)
-    void Attack()
+    //強攻撃(ジャンプ中にJキーかXボタンを入力)
+    //消費トリックはプレイヤーの最大トリックのstrong_TrickCostPercent%分消費
+    void Attack_Strong()
     {
-        if (Input.GetButtonDown("Fire1")||Input.GetKeyDown("j"))
+        trickCost = player.trickMax * strong_TrickCostPercent / 100;//強攻撃時の消費トリック
+        if ((Input.GetButtonDown("Fire1")||Input.GetKeyDown("j"))&& jumpcontrol.jumpNow == true&&trickCost<=player.trick&&enemy!=null)//JキーかXボタンを押した＆ジャンプしている＆消費トリックが足りる＆敵がいる時のみ攻撃可能
         {
-            if (jumpcontrol.jumpNow == true)
-            {
-                if (enemy != null)
-                {
-                    enemy.Damage(player.trick / player.trickMax * damageMax);
-                }
-
-                attacked = true;//攻撃した
-                player.ConsumeTRICK();
-            }
+            enemy.Damage(strong_Damage);
+            attacked = true;//攻撃した
+            player.ConsumeTRICK(trickCost);
         }
+    }
 
-        if(jumpcontrol.jumpNow == false)//水面に接地しているなら
+
+    //中攻撃(ジャンプ中にKキーかBボタンを入力)
+    //消費トリックはプレイヤーの最大トリックのstrong_TrickCostPercent%分消費
+    void Attack_Medium()
+    {
+        trickCost = player.trickMax * medium_TrickCostPercent / 100;//強攻撃時の消費トリック
+        if ((Input.GetButtonDown("Fire2") || Input.GetKeyDown("k")) && jumpcontrol.jumpNow == true && trickCost <= player.trick && enemy != null)//KキーかBボタンを押した＆ジャンプしている＆消費トリックが足りる＆敵がいる時のみ攻撃可能
+        {
+            enemy.Damage(medium_Damage);
+            attacked = true;//攻撃した
+            player.ConsumeTRICK(trickCost);
+        }
+    }
+
+
+    //弱攻撃(ジャンプ中にLキーかAボタンを入力)
+    //消費トリックはプレイヤーの最大トリックのstrong_TrickCostPercent%分消費
+    void Attack_Weak()
+    {
+        trickCost = player.trickMax * weak_TrickCostPercent / 100;//強攻撃時の消費トリック
+        if ((Input.GetButtonDown("Fire3") || Input.GetKeyDown("l")) && jumpcontrol.jumpNow == true && trickCost <= player.trick && enemy != null)//LキーかAボタンを押した＆ジャンプしている＆消費トリックが足りる＆敵がいる時のみ攻撃可能
+        {
+            enemy.Damage(weak_Damage);
+            attacked = true;//攻撃した
+            player.ConsumeTRICK(trickCost);
+        }
+    }
+
+    //ジャンプしていない時攻撃していない判定にする
+    void AttackedtoFalseNoJump()
+    {
+        if (jumpcontrol.jumpNow == false)//水面に接地しているなら
         {
             attacked = false;//攻撃していない
         }
