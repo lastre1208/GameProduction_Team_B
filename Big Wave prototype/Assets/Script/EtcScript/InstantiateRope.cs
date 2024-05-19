46f7e8fb2fbd917e104c7ab5ace278c328574fd1
deigo6125 <g2327022@outlook.jp>
2024-05-19 13:07:52 +0900

    プレイヤーと敵をつなぐ紐の実装
    紐の生成、描画を行うスクリプト2つと紐のマテリアル、紐のしなりを表現するための質点で構成されたプレハブを作成しました。

---
+++ Big Wave prototype/Assets/Script/EtcScript/InstantiateRope.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateRope : MonoBehaviour
+{
+    Enemy enemy;
+    Player player;
+
+    [SerializeField] GameObject ropePrefab;//ロープのプレハブ
+    private GameObject ropeInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.FindWithTag("Enemy").GetComponent<Enemy>();
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+        ropeInstance = Instantiate(ropePrefab);//ロープ生成
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy.hp <= 0 || player.hp <= 0)//敵かプレイヤーのhpが0以下になったら
+        {
+            Destroy(ropeInstance);//ロープ消滅
+        }
+    }
+}
