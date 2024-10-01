a9429deb9822f81dc1c250086ec11f1c78ba08a0
soltshio <shanshanshien@gmail.com>
2024-10-01 16:16:07 +0900

    追加の修正

--- Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JudgeJumpNow.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JudgeJumpNow.cs
@@ -25,15 +25,4 @@ public class JudgeJumpNow : MonoBehaviour
             jumpNow = false;//現在ジャンプしているかの状態を変える
         }
     }
-
-    
-    //地面に触れた(着地した)時、ジャンプしていない判定にする、引数のcollisionには触れたオブジェクトの情報をjumpにはfalseを入れる
-    //地面から離れた(ジャンプした)時、ジャンプした判定にする、引数のsollisionには触れたオブジェクトの情報をjumpにはtrueを入れる
-    void ChangeJumpNowStatus(Collision collison, bool jump)
-    {
-        if (collison.gameObject.CompareTag("Ground"))//触れているものが地面である
-        {
-            jumpNow = jump;//現在ジャンプしているかの状態を変える
-        }
-    }
 }
