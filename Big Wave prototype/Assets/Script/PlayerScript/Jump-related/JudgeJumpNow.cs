ae1b38f0bde28c87200828e6fc6d7b2ea3ab7d23
soltshio <shanshanshien@gmail.com>
2024-10-01 16:14:23 +0900

    ジャンプのバグを修正しました

--- Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JudgeJumpNow.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JudgeJumpNow.cs
@@ -13,16 +13,20 @@ public class JudgeJumpNow : MonoBehaviour
         return jumpNow;
     }
 
+    public void StartJump()//現在のジャンプしている状態にする
+    {
+        jumpNow = true;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        ChangeJumpNowStatus(collision,false);//地面に触れた(着地した)
+        if (collision.gameObject.CompareTag("Ground"))//触れているものが地面である
+        {
+            jumpNow = false;//現在ジャンプしているかの状態を変える
+        }
     }
 
-    void OnCollisionExit(Collision collision)
-    {
-        ChangeJumpNowStatus(collision, true);//地面から離れた(ジャンプした)
-    }
-
+    
     //地面に触れた(着地した)時、ジャンプしていない判定にする、引数のcollisionには触れたオブジェクトの情報をjumpにはfalseを入れる
     //地面から離れた(ジャンプした)時、ジャンプした判定にする、引数のsollisionには触れたオブジェクトの情報をjumpにはtrueを入れる
     void ChangeJumpNowStatus(Collision collison, bool jump)
