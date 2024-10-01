635109d2cb082e588ad4b7288bda33262876f8ff
soltshio <shanshanshien@gmail.com>
2024-10-01 17:51:27 +0900

    ホバージャンプを修正しました

--- Big Wave prototype/Assets/Script/PlayerScript/HoverJump.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/HoverJump.cs
@@ -2,38 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//作成者:桑原
+//作成者:桑原(コルーチン部分)
+//一部杉山が改造
 public class HoverJump : MonoBehaviour
 {
-    [Header("トリック使用時の滞空時間")]
-    [SerializeField] float hoverTime = 0.2f;//トリック使用時の滞空時間
-    [Header("滞空終了時に起こるジャンプの強さ")]
-    [SerializeField] float hoverJumpStrength = 5f;//滞空終了時に起こるジャンプの強さ
-    //private Coroutine coroutine;
     Rigidbody rb;
-    JudgeOnceReachedHighestPoint_Jumping judgeOnceReachedHighestPoint_Jumping;
-    // Start is called before the first frame update
+    //[Header("トリック使用時の滞空時間")]
+    //[SerializeField] float hoverTime = 0.2f;//トリック使用時の滞空時間
+    [Header("ジャンプの強さ")]
+    [SerializeField] float jumpStrength = 5f;//ジャンプの強さ
+                                                  //private Coroutine coroutine;
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        judgeOnceReachedHighestPoint_Jumping=GetComponent<JudgeOnceReachedHighestPoint_Jumping>();
     }
 
-    public void HoverDelayJump()//hoverTime秒ホバーした後ジャンプする
+    public void HoverJumpTrigger()//ホバージャンプの発動
     {
-        bool hoverJump=judgeOnceReachedHighestPoint_Jumping.Reached;//ホバージャンプするか
+        if(rb.velocity.y<=0) rb.velocity = Vector3.zero;//落ちている時は一旦落ちる速度を0にする
 
-        if(hoverJump) StartCoroutine(HoverJumpCoroutine());//落ちている時のみホバージャンプ発動
+        rb.AddForce(Vector3.up*jumpStrength,ForceMode.Impulse);
+        //rb.velocity += accelerationVector*Time.deltaTime;//加速
     }
 
-    IEnumerator HoverJumpCoroutine()//遅れてホバージャンプする
-    {
-        rb.useGravity = false;
-        rb.velocity = Vector3.zero;//重力とジャンプの運動を一時的に止める
+    //IEnumerator HoverJumpCoroutine()//遅れてホバージャンプする
+    //{
+    //    rb.useGravity = false;
+    //    rb.velocity = Vector3.zero;//重力とジャンプの運動を一時的に止める
 
-        yield return new WaitForSeconds(hoverTime);
+    //    yield return new WaitForSeconds(hoverTime);
 
-        rb.useGravity = true;
-        rb.velocity = new(0, hoverJumpStrength, 0);
-    }
+    //    rb.useGravity = true;
+    //    rb.velocity = new(0, hoverJumpStrength, 0);
+    //}
 }
