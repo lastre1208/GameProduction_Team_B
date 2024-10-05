b41be708dc86c8dbb76bbba46a26a12785c95c60
soltshio <shanshanshien@gmail.com>
2024-10-05 17:27:21 +0900

    LimitMoveコンポーネントの改良
    x軸方向だけじゃなくてy軸、z軸方向にも移動制限が可能になりました(現在プレイヤーにy軸の移動制限をかけているのでトリックのジャンプで画面外に行くことはなくなりました)

--- Big Wave prototype/Assets/Script/GamePosScript/LimitMove.cs
+++ Big Wave prototype/Assets/Script/GamePosScript/LimitMove.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+class LimitVector
+{
+    [Header("移動制限するか")]
+    [SerializeField] bool limit=false;//移動制限するか
+    [Header("移動可能範囲")]
+    [SerializeField] float range;//移動可能範囲
+
+    public float Limit(float position)
+    {
+        if (!limit) return position;
+
+        //移動制限の処理(しないならこれの前の文で処理は終わり)
+        position = Mathf.Clamp(position, -range, range);
+        return position;
+    }
+}
+
+[System.Serializable]
 class LimitMoveObject
 {
-    [Header("移動可能範囲")]
-    [SerializeField] float range = 7f;//移動可能範囲
+    [Header("移動可能のパラメータ")]
+    [SerializeField] LimitVector x;//x軸
+    [SerializeField] LimitVector y;//y軸
+    [SerializeField] LimitVector z;//z軸
     [Header("移動制限させるオブジェクト")]
     [SerializeField] GameObject limitObjects;
 
     //動きの制限
     //移動可能範囲外に出ないようにする
-    internal void Limit()
+    public void Limit()
     {
         Vector3 currentPos = limitObjects.transform.localPosition;
-        currentPos.x = Mathf.Clamp(currentPos.x, -range, range);//x軸で移動可能範囲を制限する
+        currentPos.x = x.Limit(currentPos.x);//x軸の制限
+        currentPos.y = y.Limit(currentPos.y);//y軸の制限
+        currentPos.z = z.Limit(currentPos.z);//z軸の制限
         limitObjects.transform.localPosition = currentPos;
     }
 }
