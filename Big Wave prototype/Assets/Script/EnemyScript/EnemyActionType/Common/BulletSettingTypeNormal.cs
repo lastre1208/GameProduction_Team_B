cf190196119d19776523dace29ec6a2f12069450
soltshio <shanshanshien@gmail.com>
2024-10-08 10:41:01 +0900

    敵の弾を撃つスクリプトの修正(まだ途中)

---
+++ Big Wave prototype/Assets/Script/EnemyScript/EnemyActionType/Common/BulletSettingTypeNormal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ShotType_E
+{
+    forward,
+    toPlayer
+}
+
+public class BulletSettingTypeNormal : BulletSettingTypeBase
+{
+    [Header("ËŒ‚‚Ìí—Ş")]
+    [SerializeField] ShotType_E shotType;
+    [Header("¥Œ‚‚Â—Í")]
+    [SerializeField] float shotPower;//Œ‚‚Â—Í
+
+    public ShotType_E ShotType { get { return shotType; } }
+    public float ShotPower { get { return shotPower; } }
+}
