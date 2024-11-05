6ca03cbb8450f28aeb8ffd6ee01c4f9687e29fcf
soltshio <shanshanshien@gmail.com>
2024-11-05 15:34:45 +0900

    コンボの表示のスクリプトとCinemachineの拡張(角度固定)スクリプトの修正

--- Big Wave prototype/Assets/Script/CinemachineScripts/CinemachineExtend_Look.cs
+++ Big Wave prototype/Assets/Script/CinemachineScripts/CinemachineExtend_Look.cs
@@ -7,6 +7,13 @@ using UnityEngine;
 //指定オブジェクトとy軸とz軸を同じ方向を向かせる
 public class CinemachineExtend_Look : CinemachineExtension
 {
+    [Header("追従目標")]
+    [SerializeField] Transform target;
+    [Header("固定する軸")]
+    [SerializeField] bool x;
+    [SerializeField] bool y;
+    [SerializeField] bool z;
+
     [Header("これに入れたオブジェクトとy軸のみ常に同じ方向を向くようになる")]
     [SerializeField] Transform lookObj;
 
@@ -22,14 +29,23 @@ public class CinemachineExtend_Look : CinemachineExtension
         if (stage != CinemachineCore.Stage.Aim)
             return;
 
-        //y軸とz軸を同じ方向を向かせる
+        //チェックを入れた固定軸に対して固定する
         var eulerAngles = state.RawOrientation.eulerAngles;
-        eulerAngles.y=lookObj.eulerAngles.y;
-        eulerAngles.z = 0;
+        if (x) eulerAngles.x = target.eulerAngles.x;//x軸の固定
+        if (y) eulerAngles.y = target.eulerAngles.y;//x軸の固定
+        if (z) eulerAngles.z = target.eulerAngles.z;//x軸の固定
         state.RawOrientation = Quaternion.Euler(eulerAngles);
+
+        //y軸とz軸を同じ方向を向かせる
+        //var eulerAngles = state.RawOrientation.eulerAngles;
+        //eulerAngles.y=lookObj.eulerAngles.y;
+        //eulerAngles.z = 0;
+        //state.RawOrientation = Quaternion.Euler(eulerAngles);
         //Quaternion rot = state.RawOrientation;
         //rot.y=lookObj.rotation.y;
         //rot.z=0;
         //state.RawOrientation = rot;
     }
+
+    
 }
