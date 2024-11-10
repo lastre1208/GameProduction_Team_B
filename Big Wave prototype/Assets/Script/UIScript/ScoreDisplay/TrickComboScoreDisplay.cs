e6c44bb909168cedc627f0e15df534a3dc79957f
soltshio <shanshanshien@gmail.com>
2024-11-10 13:35:06 +0900

    ゲームプレイ中のスコアの表示を修正しました

--- Big Wave prototype/Assets/Script/UIScript/ScoreDisplay/TrickComboScoreDisplay.cs
+++ Big Wave prototype/Assets/Script/UIScript/ScoreDisplay/TrickComboScoreDisplay.cs
@@ -5,13 +5,15 @@ using UnityEditor.Rendering;
 using UnityEngine;
 
 //作成者:杉山
-//ゲーム中にトリックコンボのスコアを表示させる
+//ゲーム中にスコア(トリックコンボとチャージ時間)を表示させる
 public class TrickComboScoreDisplay : MonoBehaviour
 {
     [Header("スコア表示する文字")]
     [SerializeField] TMP_Text m_scoreText;
-    [Header("チャージ時間のスコアを計測するコンポーネント")]
+    [Header("トリックコンボのスコアを計測するコンポーネント")]
     [SerializeField] ScoreGameScene_TrickCombo score_TrickCombo;
+    [Header("チャージ時間のスコアを計測するコンポーネント")]
+    [SerializeField] ScoreGameScene_ChargeTime score_ChargeTime;
     [Header("表示する文字の増えるスピード")]
     [SerializeField] float m_riseSpeed;
     private float displayScore=0;//表示用のスコア(増える際、瞬間的に数字を切り替えるのではなくナンバーカウンターのように増やす演出実装のため)
@@ -24,9 +26,12 @@ public class TrickComboScoreDisplay : MonoBehaviour
 
     void Display()//表示する
     {
+        //現在のスコア
+        float score = score_TrickCombo.Score+score_ChargeTime.Score;
+
         //現在のスコアになるまで増え続ける
         displayScore += Time.deltaTime*m_riseSpeed;
-        displayScore = Mathf.Clamp(displayScore, defaultScore, score_TrickCombo.Score);
+        displayScore = Mathf.Clamp(displayScore, defaultScore, score);
 
         m_scoreText.text = displayScore.ToString("0");
     }
