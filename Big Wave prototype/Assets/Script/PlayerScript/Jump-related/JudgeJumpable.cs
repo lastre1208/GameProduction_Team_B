94cd14c40bbd6a543b8f373417a9be95cb04f9e6
soltshio <shanshanshien@gmail.com>
2024-11-24 18:15:02 +0900

    ジャンプゲージの挙動の変更とジャンプ力のチャージの条件を少し変更
    ジャンプゲージがプレイヤーに追従するようになりました。また、チャージしていない時は非表示になるようにしました。
    ジャンプできない状態になった時にジャンプ力がリセットされるようになりました

--- Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JudgeJumpable.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JudgeJumpable.cs
@@ -8,6 +8,8 @@ using System;
 public class JudgeJumpable : MonoBehaviour
 {
     public event Action<bool> SwitchJumpable;//ジャンプ可能かが切り替わる瞬間に呼ぶ、trueならジャンプできるようになった時、falseならジャンプできなくなった時
+    public event Action ToJumpable;//ジャンプ可能になった時に呼ぶ
+    public event Action ToNotJumpable;//ジャンプ不可能になった時に呼ぶ
     [SerializeField] JudgeJumpNow _judgeJumpNow;
     [SerializeField] JudgeTouchWave _judgeTouchWave;
     bool _jumpableBeforeFrame = false;//前フレームのジャンプ可能状態
@@ -26,8 +28,19 @@ public class JudgeJumpable : MonoBehaviour
     {
         if(_jumpableBeforeFrame!=Jumpable)//ジャンプ可能状態が切り替わった時に切り替わりの瞬間の処理を呼ぶ
         {
-            bool switchJumpable = !_jumpableBeforeFrame ? true : false;//前フレームでジャンプ不可能であれば ジャンプ可能になったということ
+            bool switchJumpable = !_jumpableBeforeFrame;//前フレームでジャンプ不可能であれば ジャンプ可能になったということ
+
+            //ジャンプ可能状況の切り替わり時の処理を呼ぶ
             SwitchJumpable?.Invoke(switchJumpable);
+
+            if(switchJumpable)
+            {
+                ToJumpable?.Invoke();
+            }
+            else
+            {
+                ToNotJumpable?.Invoke();
+            }
         }
 
         _jumpableBeforeFrame = Jumpable;//前フレームのジャンプ可能状態の更新
--- Big Wave prototype/Assets/Script/UIScript/JumpPowerDisplay/JumpPowerDisplay.cs
+++ Big Wave prototype/Assets/Script/UIScript/JumpPowerDisplay/JumpPowerDisplay.cs
@@ -7,19 +7,29 @@ using UnityEngine.UI;
 //ジャンプ力をUIで表示
 public class JumpPowerDisplay : MonoBehaviour
 {
-    [Header("ジャンプ力ゲージ")]
-    [SerializeField] Image jumpPowerGauge;//ジャンプ力ゲージ
+    [Header("ジャンプ力ゲージ(非チャージ時に隠す部分)")]
+    [SerializeField] GameObject main_jumpPowerGauge;//ジャンプ力ゲージ(非チャージ時に隠す部分)
+    [Header("ジャンプ力ゲージ可変部分")]
+    [SerializeField] Image jumpPowerGauge;//ジャンプ力ゲージ可変部分
     [Header("ジャンプ力")]
     [SerializeField] JumpPower jumpPower;
 
     void Update()
     {
+        Display();
         JumpPowerGauge();
     }
 
     void JumpPowerGauge()//ジャンプ力ゲージの処理
     {
+        if (!main_jumpPowerGauge.activeSelf) return;//ジャンプ力ゲージが非表示の時は以下の処理をしない
+
         float ratio = jumpPower.Ratio;
         jumpPowerGauge.fillAmount = ratio;
+    }
+
+    void Display()//チャージ中のみ表示させる
+    {
+        main_jumpPowerGauge.SetActive(jumpPower.ChargeNow);
     }
 }
