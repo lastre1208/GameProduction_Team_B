9b63c8dcdbe2797306d34efe20d7ceb34bb02393
lastre1208 <lastobserver.1208@gmsail.com>
2024-11-24 18:23:05 +0900

    攻撃着弾時の演出を状態に合わせて変えられるようにしました

--- Big Wave prototype/Assets/Script/PlayerScript/AlongWay/Land.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/AlongWay/Land.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //作成者:杉山
 //着弾時の効果音と敵の被ダメモーションのセットの処理を一時的に避難させてます
@@ -8,18 +9,32 @@ public class Land : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] AudioSource source;
-    [SerializeField] AudioClip clip;
+    [SerializeField] AudioClip clip_Critical;
+    [SerializeField] AudioClip clip_Normal;
+    [SerializeField] AudioClip clip_Fever;
     [SerializeField] Generate_AlongWay generate_AlongWay;
 
     void Start()
     {
+        generate_AlongWay.NormalTrickEffect.LandAction += LandEffect_Normal;
         generate_AlongWay.CriticalTrickEffect.LandAction += LandEffect;
-        generate_AlongWay.CriticalFeverTrickEffect.LandAction += LandEffect;
+        generate_AlongWay.CriticalFeverTrickEffect.LandAction += LandEffect_Fever;
     }
 
-    public void LandEffect()
+     public void LandEffect_Normal()//通常時
+    {
+        source.PlayOneShot(clip_Normal);
+    }
+    public void LandEffect()//クリティカル時
+    {
+        
+        animator.SetTrigger("Damage");
+        source.PlayOneShot(clip_Critical);
+    }
+    public void LandEffect_Fever()//フィーバー時
     {
         animator.SetTrigger("Damage");
-        source.PlayOneShot(clip);
+        source.PlayOneShot(clip_Fever);
     }
+   
 }
