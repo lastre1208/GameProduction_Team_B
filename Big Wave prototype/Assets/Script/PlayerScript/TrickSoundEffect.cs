f4a50bc9f7d027343a1b12845a25d6fe2b98fa80
lastre1208 <lastobserver.1208@gmsail.com>
2024-11-28 17:55:31 +0900

    トリックガイドを新しくしました

--- Big Wave prototype/Assets/Script/PlayerScript/TrickSoundEffect.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/TrickSoundEffect.cs
@@ -8,13 +8,30 @@ public class TrickSoundEffect : MonoBehaviour
     [SerializeField] AudioClip criticalSE;//クリティカル時の効果音
     [Header("通常状態の効果音")]
     [SerializeField] AudioClip normalSE;//通常状態の効果音
+    [Header("最大ピッチ")]
+    [SerializeField] float pitchMax;
+    [Header("上昇ピッチ量")]
+    [SerializeField] float pitchUp;
     [Header("必要なコンポーネント")]
     [SerializeField] AudioSource audioSource;
     [SerializeField] Critical critical;
-
+  
+    private float startPitch;
+    private void Start()
+    {
+       startPitch= audioSource.pitch;
+    }
     public void PlayTrickSE()//クリティカル状況によって鳴らす音を変える
     {
         AudioClip se=critical.CriticalNow ? criticalSE: normalSE;
+        if (critical.CriticalNow&&audioSource.pitch<pitchMax)
+        {
+            audioSource.pitch += pitchUp;
+        }
+        else if(!critical.CriticalNow)
+        {
+          audioSource.pitch =startPitch ;
+        }
         audioSource.PlayOneShot(se);
     }
 }
