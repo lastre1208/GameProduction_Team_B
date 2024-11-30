2ccbe6c67fa9ad8878c46f3c1530a4887d077352
soltshio <shanshanshien@gmail.com>
2024-11-30 15:13:43 +0900

    一旦保存

--- Big Wave prototype/Assets/Script/UIScript/ChaseObjectOnUI.cs
+++ Big Wave prototype/Assets/Script/UIScript/ChaseObjectOnUI.cs
@@ -2,20 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//作成者:杉山
+//ターゲットを追いかけるUI
 public class ChaseObjectOnUI : MonoBehaviour
 {
-    [Header("UIが追いかける対象")]
-    [SerializeField] Transform targetTfm;//UIが追いかける対象
-    private RectTransform myRectTfm;
+    [Header("オブジェクトを移すカメラ")]
+    [SerializeField] Camera _targetCamera;
+    [Header("追いかける対象")]
+    [SerializeField] private Transform _target;
+    private RectTransform _parentUI;//親の位置
 
-    // Start is called before the first frame update
     void Start()
     {
-        myRectTfm = GetComponent<RectTransform>();
+        // カメラが指定されていなければメインカメラにする
+        if (_targetCamera == null)
+            _targetCamera = Camera.main;
+
+        // 親UIのRectTransformを保持
+        _parentUI = transform.parent.GetComponent<RectTransform>();
     }
 
     void Update()
     {
-        myRectTfm.position = RectTransformUtility.WorldToScreenPoint(Camera.main, targetTfm.position);
+        UpdateUIPos();
+    }
+
+    void UpdateUIPos()//UIの位置を更新
+    {
+        // オブジェクトの位置
+        var targetWorldPos = _target.position;
+       
+        if (!TargetIsFront(targetWorldPos)) return;
+
+        // オブジェクトのワールド座標→スクリーン座標変換
+        var targetScreenPos = _targetCamera.WorldToScreenPoint(targetWorldPos);
+
+        // スクリーン座標変換→UIローカル座標変換
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _parentUI,
+            targetScreenPos,
+            null,//CanvasのrenderModeがOverrayの時はnullにする
+            out var uiLocalPos
+        );
+
+        // RectTransformのローカル座標を更新
+        transform.localPosition = uiLocalPos;
+    }
+
+    bool TargetIsFront(Vector3 targetWorldPos)//ターゲットがカメラの前にいるか
+    {
+        Transform cameraTransform = _targetCamera.transform;
+
+        // カメラの向きベクトル
+        Vector3 cameraDir = cameraTransform.forward;
+
+        // カメラからターゲットへのベクトル
+        Vector3 targetDir = targetWorldPos - cameraTransform.position;
+
+        // 内積を使ってカメラ前方かどうかを判定
+        bool isFront = Vector3.Dot(cameraDir, targetDir) > 0;
+
+        // カメラ前方ならUI表示、後方なら非表示
+        gameObject.SetActive(isFront);
+
+        return isFront;
     }
 }
