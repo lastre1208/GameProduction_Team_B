5023219878ecc340e4aef0d15109ec010474e95d
lastre1208 <lastobserver.1208@gmsail.com>
2024-12-03 17:51:02 +0900

    各種エフェクトの追加

--- Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JumpEffect.cs
+++ Big Wave prototype/Assets/Script/PlayerScript/Jump-related/JumpEffect.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class JumpEffect : MonoBehaviour
 {
+    [SerializeField] GameObject _target;
     [SerializeField] JudgeJumpNow _judgeJumpNow;
     [SerializeField] GameObject _waterSplashEffect;
+    [SerializeField] GameObject _jumpEffect;
     [SerializeField] AudioSource _audioSource;
     [SerializeField] AudioClip _jumpSE;
-
+    [SerializeField] float _effectOffset;
+    GameObject EffectPrefab;
     void Start()
     {
         _judgeJumpNow.SwitchJumpNowAction += Effect;
@@ -21,6 +24,8 @@ public class JumpEffect : MonoBehaviour
         //ジャンプ開始
         if (switchJumpNow)
         {
+            Vector3 E_position=new(_target.transform.position.x,_target.transform.position.y-_effectOffset,_target.transform.position.z);
+        EffectPrefab=Instantiate(_jumpEffect,E_position,Quaternion.Euler(-90,0,0),_target.transform);
             _audioSource.PlayOneShot(_jumpSE);//ジャンプの効果音を鳴らす
         }
         //着地時
