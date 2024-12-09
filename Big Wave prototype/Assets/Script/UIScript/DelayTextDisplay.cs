00b7348d8e66d672996c20a8f071b2842fc4a709
lastre1208 <lastobserver.1208@gmsail.com>
2024-12-09 15:37:20 +0900

    行動パターンの調整

--- Big Wave prototype/Assets/Script/UIScript/DelayTextDisplay.cs
+++ Big Wave prototype/Assets/Script/UIScript/DelayTextDisplay.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
+using UnityEngine.UI;
 using TMPro;
 public class DelayTextDisplay : MonoBehaviour
 {
     [SerializeField] TMP_Text _text;
     [SerializeField] float _delay;
+    [SerializeField] AudioSource _audio;
+    [SerializeField] AudioClip _clip;
     private string _delayText;
     // Start is called before the first frame update
     void Start()
@@ -18,10 +20,43 @@ public class DelayTextDisplay : MonoBehaviour
 
    IEnumerator DelayDisplay()
     {
-        foreach(char c in _delayText)
+
+        bool isInsideTag = false; // タグ部分をスキップするためのフラグ
+        string currentText = "";
+
+        foreach (char c in _delayText)
         {
-            _text.text += c;
-            yield return new WaitForSeconds(_delay);
+
+            if (c == '<') // タグの開始
+            {
+                isInsideTag = true;
+                currentText += c; // タグも一緒に蓄積
+            }
+            else if (c == '>') // タグの終了
+            {
+                isInsideTag = false;
+                currentText += c; // タグを完成させる
+            }
+            else if (isInsideTag)
+            {
+                currentText += c; // タグ内の文字をそのまま蓄積
+            }
+            else
+            {
+                currentText += c; // 通常の文字を追加
+                _text.text = currentText; // 表示を更新
+
+                yield return new WaitForSeconds(_delay);
+                if (c != ' ' && c != '!')
+                {
+                    if (_audio != null)
+                    {
+                        _audio.PlayOneShot(_clip);
+                    }
+
+                }
+
+            }
         }
     }
    
