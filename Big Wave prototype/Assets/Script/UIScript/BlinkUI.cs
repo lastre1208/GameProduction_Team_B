13e991b1009092be11427e8aa142ba7c086f97b1
lastre1208 <lastobserver.1208@gmsail.com>
2024-12-31 23:57:58 +0900

    敵の攻撃パターン調整完了

--- Big Wave prototype/Assets/Script/UIScript/BlinkUI.cs
+++ Big Wave prototype/Assets/Script/UIScript/BlinkUI.cs
@@ -2,38 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using JetBrains.Annotations;
+
 public class BlinkUI : MonoBehaviour
 {
     private TMP_Text T_Color;
-    [SerializeField] float cycle;
-    [SerializeField] JumpUI jump;
-   
+    [SerializeField] float cycle = 1f; // 点滅の間隔
+    [SerializeField] JumpUI jump; // 外部から設定されるJumpUIクラス
+
     private float time;
+
     private void Start()
     {
-         T_Color=this.GetComponent<TMP_Text>();
-        T_Color.enabled = true; 
-      
+        // TMP_Text コンポーネントを取得
+        T_Color = this.GetComponent<TMP_Text>();
+        T_Color.enabled = true;
     }
-    // Update is called once per frame
+
+    // 毎フレーム実行
     void Update()
     {
         Blink_UI();
     }
+
+    // 点滅ロジック
     void Blink_UI()
     {
-        time += Time.deltaTime;
-       
-        if (time > cycle && !jump.reached)
+        if (jump != null && jump.reached)
         {
-            time = 0;
-            T_Color.enabled = !T_Color.enabled;
-        }
-        else
-        {
+            // 点滅を停止し、常に表示状態にする
             T_Color.enabled = true;
+            return;
+        }
+
+        // 時間を加算
+        time += Time.deltaTime;
+
+        // cycleごとに点滅を切り替え
+        if (time > cycle)
+        {
+            time = 0f; // 時間をリセット
+            T_Color.enabled = !T_Color.enabled; // 表示/非表示を切り替え
         }
     }
-   
 }
